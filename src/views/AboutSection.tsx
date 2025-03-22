12e777e6f33e6cadf70efee8fd57d9dc82340196
Tanadon Hunsadee <tanadonhun.dev@gmail.com>
2025-03-22 23:47:04 +0700

    refactor AboutSection

--- src/views/AboutSection.tsx
+++ src/views/AboutSection.tsx
@@ -5,14 +5,27 @@ export default function AboutSection() {
     <div>
       <section id="about" className="py-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <h2 className="text-3xl font-bold text-center mb-8 text-yellow-600">
-            About Me
-          </h2>
-          <p className="text-lg text-center mb-8">
-            I graduated from King Mongkut's University of Technology North
-            Bangkok, Still have little work experience but like to learn new
-            things so that the organization can develop.
-          </p>
+          <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 h-screen flex flex-col md:flex-row items-center justify-between">
+            {/* คอลัมน์รูปภาพ */}
+            <div className="mt-8 md:mt-0 md:w-10/12 flex justify-center">
+              <img
+                src="/profile1.jpg"
+                alt="Placeholder Image"
+                className="rounded-lg shadow-lg max-w-full h-auto"
+              />
+            </div>
+            {/* คอลัมน์ข้อความ */}
+            <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col items-center justify-center text-center h-screen">
+              <h2 className="text-3xl font-bold text-yellow-600 mb-4">
+                About Me
+              </h2>
+              <p className="text-lg">
+                I graduated from King Mongkut's University of Technology North
+                Bangkok, Still have little work experience but like to learn new
+                things so that the organization can develop.
+              </p>
+            </div>
+          </div>
         </div>
       </section>
     </div>
